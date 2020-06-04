6e21f3ed4f87586ddff1c2427af2fd267ab7e285
Pimenov Mikhail <pimenov57@gmail.com>
2020-06-04 13:57:24 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMO_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var name = "";
+            Console.WriteLine("Привет программа!");
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет {name}");
+        }
+    }
+}
